340c13c1596a1ebc2dc810c277cbea777ccc407f
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-04 14:57:38 -0300

    Implementa FactoryPattern na ConexaoDB

---
+++ src/main/java/org/example/conexaoDB/FactoryConexaoDB.java
@@ -0,0 +1,19 @@
+package org.example.conexaoDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class FactoryConexaoDB {
+
+    public Connection recuperaConexao(String password){
+        try{
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=" + password);
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
