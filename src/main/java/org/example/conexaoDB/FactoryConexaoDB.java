be076a5afba8e8bfff9de0078d347c0de9b8e5dd
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-04 14:57:59 -0300

    exclui import do Scanner

--- src/main/java/org/example/conexaoDB/FactoryConexaoDB.java
+++ src/main/java/org/example/conexaoDB/FactoryConexaoDB.java
@@ -3,7 +3,6 @@ package org.example.conexaoDB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class FactoryConexaoDB {
 
