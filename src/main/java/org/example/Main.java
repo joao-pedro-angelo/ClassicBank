8380d241266eff3a4eddd58ab6ffd7a70bcc7ac4
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-06 12:39:03 -0300

    Classe Main - Documentação

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,27 @@ import org.example.services.ContaService;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
+/**
+ * Classe que realiza a interação com o usuário
+ * Entrada e saída de dados
+ *
+ * @author carneiro.angelo.joao.pedro@gmail.com
+ */
 public class Main {
 
     private static final Scanner sc = new Scanner(System.in).useDelimiter("\n");
     private static final ContaService contaService = new ContaService();
 
+
+    /**
+     * Método main - ponto de partida de uma aplicação Java
+     *
+     * Realiza o controle de seleção de método.
+     * Os métodos invocados podem lançar a exceção RegraDeNegocioException(), que é capturada
+     * por este método.
+     *
+     * @param args Parâmetro padrão do método main
+     */
     public static void main(String[] args){
         int opcao = exibirMenu();
         while (opcao != 8){
@@ -47,6 +63,15 @@ public class Main {
     }
 
 
+    /**
+     * Exibe o menu e retorna a escolha do usuário (um inteiro).
+     *
+     * Caso o usuário digite algo que não seja um número inteiro, a exceção será capturada
+     * e o retorno será 8, que encerra a aplicação.
+     *
+     * @return O inteiro digitado pelo usuário. Ou 8, em caso de exceção capturada,
+     * encerrando a aplicação.
+     */
     private static int exibirMenu(){
         System.out.println("""
                 CLASSIC BANK - ESCOLHA UMA OPÇÃO:
@@ -64,11 +89,14 @@ public class Main {
             return Integer.parseInt(sc.nextLine());
         } catch (NumberFormatException e){
             System.out.println("Entrada inválida...");
-            return 7;
+            return 8;
         }
     }
 
 
+    /**
+     * Exibe todas as contas cadastradas no sistema.
+     */
     private static void listarContas(){
         System.out.println("Contas cadastradas: ");
         System.out.println(contaService.listarContas());
@@ -78,6 +106,12 @@ public class Main {
     }
 
 
+    /**
+     * Captura as informações necessárias para a criação de uma conta.
+     * Passa essas informações para outro método que é quem irá abrir a conta.
+     *
+     * As informações serão checadas em outros métodos.
+     */
     private static void abrirConta(){
         System.out.println("Digite o número da conta: ");
         Integer numeroConta = Integer.parseInt(sc.nextLine());
@@ -94,6 +128,11 @@ public class Main {
     }
 
 
+    /**
+     * Captura as informações necessárias para encerrar uma conta.
+     * Passa essas informações para um outro método.
+     * As validações (verificação das entradas) serão feitas em outros métodos.
+     */
     private static void encerrarConta(){
         System.out.println("Digite o número da conta:");
         Integer numeroDaConta = Integer.parseInt(sc.nextLine());
@@ -107,6 +146,11 @@ public class Main {
     }
 
 
+    /**
+     * Captura as informações necessárias para consultar o saldo de uma conta.
+     * Passa essas informações para um outro método.
+     * Os dados serão checados (validados) em outro método.
+     */
     private static void consultarSaldo(){
         System.out.println("Digite o número da conta:");
         Integer numeroDaConta = Integer.parseInt(sc.nextLine());
@@ -119,6 +163,11 @@ public class Main {
     }
 
 
+    /**
+     * Captura as informações necessárias para realizar o saque em uma conta.
+     * Esses dados são passados para outros métodos.
+     * As validações são feitas em outro método.
+     */
     private static void realizarSaque(){
         System.out.println("Digite o número da conta:");
         Integer numeroDaConta = Integer.parseInt(sc.nextLine());
@@ -135,6 +184,11 @@ public class Main {
     }
 
 
+    /**
+     * Captura as informações necessárias para realizar o depósito em uma conta.
+     * Esses dados são passados para outros métodos.
+     * As validações são feitas em outro método.
+     */
     private static void realizarDeposito(){
         System.out.println("Digite o número da conta:");
         Integer numeroDaConta = Integer.parseInt(sc.nextLine());
@@ -150,6 +204,11 @@ public class Main {
         sc.nextLine();
     }
 
+
+    /**
+     * Captura as informações necessárias para consultar uma única conta.
+     * Os dados são passados e validados em outro método.
+     */
     private static void consultarContaPorNumero(){
         System.out.println("Digite o número da conta:");
         Integer numeroDaConta = Integer.parseInt(sc.nextLine());
