e20e6abe419be0f7f838f5f30fee59cdc469a2e6
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-06 12:54:00 -0300

    Exception - documentação

--- src/main/java/org/example/exception/RegraDeNegocioException.java
+++ src/main/java/org/example/exception/RegraDeNegocioException.java
@@ -1,7 +1,18 @@
 package org.example.exception;
 
+/**
+ * Classe que representa uma exceção criada para o contexto deste sistema.
+ * Deve ser lançada a exceção RegraDeNegocioException quando alguma lógica/regra do sistema
+ * for quebrada
+ *
+ * @author carneiro.angelo.joao.pedro@gmail.com
+ */
 public class RegraDeNegocioException extends RuntimeException {
 
+    /**
+     * Lança a exceção
+     * @param mensagem Mensagem de erro
+     */
     public RegraDeNegocioException(String mensagem){
         super(mensagem);
     }
